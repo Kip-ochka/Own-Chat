91922add8e07e1cfce5f793071f0a46d82d513e5
Kip-ochka <kolpakovigorjob@yandex.ru>
2024-05-26 19:51:02 +0300

    finish tests

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,7 +1,8 @@
 import { JSDOM } from "jsdom";
 
-const jsdom = new JSDOM(`<body></body>`);
+const jsdom = new JSDOM('<body></body>', {url:'http://localhost'})
 global.window = jsdom.window;
+global.XMLHttpRequest = jsdom.window.XMLHttpRequest
 global.document = jsdom.window.document;
 global.Node = jsdom.window.Node;
 global.MouseEvent = jsdom.window.MouseEvent;
